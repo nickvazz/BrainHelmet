2249cc75480581dae782eedc2cae9534f4b7a2e9
Ryan <ryan.sudhakaran@sjsu.edu>
2017-06-24 04:03:28 -0700

    Added new Arduino script!

---
+++ ard_out_2.py
@@ -0,0 +1,110 @@
+"""
+ldr.py
+Display analog data from Arduino using Python (matplotlib)
+Author: Mahesh Venkitachalam
+Website: electronut.in
+"""
+
+import sys, serial, argparse
+import numpy as np
+from time import sleep
+from collections import deque
+
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+
+
+# plot class
+class AnalogPlot:
+  # constr
+  def __init__(self, strPort, maxLen):
+      # open serial port
+      self.ser = serial.Serial(strPort, 9600)
+
+      self.ax = deque([0.0]*maxLen)
+      #self.ay = deque([0.0]*maxLen)
+      self.maxLen = maxLen
+
+  # add to buffer
+  def addToBuf(self, buf, val):
+      if len(buf) < self.maxLen:
+          buf.append(val)
+      else:
+          buf.pop()
+          buf.appendleft(val)
+
+  # add data
+  def add(self, data):
+      #assert(len(data) == 1)
+      self.addToBuf(self.ax, data)
+      #self.addToBuf(self.ay, data[0])
+
+  # update plot
+  def update(self, frameNum, a0):
+      try:
+          line = self.ser.readline()
+          #print type(line)
+          #data =  [x.rstrip('\r\n') for x in line]
+          try:
+              #data = [float(val) for val in line]
+              data =  float(line.strip())
+          except:
+              data = None
+          print data
+          # print data
+          if(data):#if(len(data) == 1):
+              self.add(data)
+              a0.set_data(range(self.maxLen), self.ax)
+              #a1.set_data(range(self.maxLen), self.ay)
+      except KeyboardInterrupt:
+          print('exiting')
+
+      return a0
+
+  # clean up
+  def close(self):
+      # close serial
+      self.ser.flush()
+      self.ser.close()
+
+# main() function
+def main():
+  # create parser
+  ##parser = argparse.ArgumentParser(description="LDR serial")
+  # add expected arguments
+  ##parser.add_argument('--port', dest='port', required=True)
+
+  # parse args
+  ##args = parser.parse_args()
+
+  strPort = '/dev/ttyACM0'
+  #strPort = args.port
+
+  print('reading from serial port %s...' % strPort)
+
+  # plot parameters
+  analogPlot = AnalogPlot(strPort, 1600)
+
+  print('plotting data...')
+
+  # set up animation
+  fig = plt.figure()
+  ax = plt.axes(xlim=(0, 1600), ylim=(0, 500))
+  a0 = ax.plot([], [])
+  #a1, = ax.plot([], [])
+  anim = animation.FuncAnimation(fig, analogPlot.update,
+                                 fargs=(a0),
+                                 interval=50)
+
+  # show plot
+  plt.show()
+
+  # clean up
+  analogPlot.close()
+
+  print('exiting.')
+
+
+# call main
+if __name__ == '__main__':
+  main()
